b5528cf350ac0d95e392e7761c7a7e097cfad366
NtandoDes <134165057+NtandoDes@users.noreply.github.com>
2025-02-16 18:10:22 +0200

    Update urls.py

--- Ispani/backend/backend/urls.py
+++ Ispani/backend/backend/urls.py
@@ -1,13 +1,11 @@
+
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from myapp.views import *
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('signup/', SignUpView.as_view(), name='signup'),
-    path('signin/', SignInView.as_view(), name='signin'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('edit_profile/', EditProfileView.as_view(), name='edit_profile'), 
+    path('', include('app.urls')), 
+    
 ]
+
