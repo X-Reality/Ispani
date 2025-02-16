d0814ae646cda52cfee4419fcf2c9047d5f51f8e
NtandoDes <134165057+NtandoDes@users.noreply.github.com>
2025-02-16 18:13:06 +0200

    Create urls.py

---
+++ Ispani/backend/myapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+from app.views import * 
+from . import consumers
+
+
+urlpatterns = [
+    path('signup/', StudentProfileListCreate.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('groups/', GroupListCreate.as_view(), name='group-list-create'),
+    path('messages/', MessageListCreate.as_view(), name='message-list-create'),
+    path('join-group/', join_group, name='join-group'),
+]
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<group_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
+]
