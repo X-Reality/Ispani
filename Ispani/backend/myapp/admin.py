fa9b503f3004b965c136aa2c781e182621dae5f5
NtandoDes <134165057+NtandoDes@users.noreply.github.com>
2025-02-16 18:11:21 +0200

    Update admin.py

--- Ispani/backend/myapp/admin.py
+++ Ispani/backend/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Group, StudentProfile
 
-# Register your models here.
+admin.site.register(StudentProfile)
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year_of_study', 'field_of_study']
+    filter_horizontal = ['members']  # To easily add members from the admin UI
+
+admin.site.register(Group, GroupAdmin)
