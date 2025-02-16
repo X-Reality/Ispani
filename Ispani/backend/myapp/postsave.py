ba2507cb54f773e3aa3cb6af914bac2e47208a00
NtandoDes <134165057+NtandoDes@users.noreply.github.com>
2025-02-16 18:13:46 +0200

    Create postsave.py

---
+++ Ispani/backend/myapp/postsave.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import StudentProfile, Group
+
+@receiver(post_save, sender=StudentProfile)
+def assign_student_to_group(sender, instance, created, **kwargs):
+    if created:  # Only when a new student profile is created
+        # Find the group(s) that match the student's year_of_study and field_of_study
+        
+        matching_groups = Group.objects.filter(
+            year_of_study=instance.year_of_study,
+            field_of_study=instance.field_of_study
+        )
+        
+        # Add the student to each matching group
+        for group in matching_groups:
+            group.members.add(instance)
