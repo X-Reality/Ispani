32fc42908d553dc7be22094b97eb52202e4fc526
Ntando <masimulantandoyenkosi195@gmail.com>
2025-02-17 13:01:52 +0200

    Added Tutoring booking system

--- Ispani/backend/myapp/admin.py
+++ Ispani/backend/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Group, StudentProfile
+from .models import Group, StudentProfile, TutorProfile
 
 admin.site.register(StudentProfile)
 
@@ -8,3 +8,19 @@ class GroupAdmin(admin.ModelAdmin):
     filter_horizontal = ['members']  # To easily add members from the admin UI
 
 admin.site.register(Group, GroupAdmin)
+
+class TutorProfileAdmin(admin.ModelAdmin):
+    list_display = ('tutor', 'is_approved', 'available_times')
+    list_filter = ('is_approved',)
+    search_fields = ('tutor__username',)
+
+    # Provide a custom action to approve multiple tutors
+    actions = ['approve_tutors']
+
+    def approve_tutors(self, request, queryset):
+        # This custom action will allow an admin to approve selected tutors
+        queryset.update(is_approved=True)
+
+    approve_tutors.short_description = "Approve selected tutors"
+
+admin.site.register(TutorProfile, TutorProfileAdmin)
