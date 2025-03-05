5baa53cd133e78f8eead3ec1f57e63a7facdc51a
Ntando <masimulantandoyenkosi195@gmail.com>
2025-03-05 20:23:58 +0200

    Integrated backend with frontend

--- Ispani/backend/myapp/admin.py
+++ Ispani/backend/myapp/admin.py
@@ -1,26 +1,14 @@
 from django.contrib import admin
-from .models import Group, StudentProfile, TutorProfile
+from .models import (
+    CustomUser, OTP, Registration, Group, Message,
+    SubjectSpecialization, TutorProfile, TutoringSession, Booking
+)
 
-admin.site.register(StudentProfile)
+# Register CustomUser
+admin.site.register(CustomUser)
 
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ['name', 'year_of_study', 'field_of_study']
-    filter_horizontal = ['members']  # To easily add members from the admin UI
 
-admin.site.register(Group, GroupAdmin)
 
-class TutorProfileAdmin(admin.ModelAdmin):
-    list_display = ('tutor', 'is_approved', 'available_times')
-    list_filter = ('is_approved',)
-    search_fields = ('tutor__username',)
 
-    # Provide a custom action to approve multiple tutors
-    actions = ['approve_tutors']
 
-    def approve_tutors(self, request, queryset):
-        # This custom action will allow an admin to approve selected tutors
-        queryset.update(is_approved=True)
 
-    approve_tutors.short_description = "Approve selected tutors"
-
-admin.site.register(TutorProfile, TutorProfileAdmin)
