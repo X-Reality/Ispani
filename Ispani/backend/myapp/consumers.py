b93868751dcacc00f4b36203b8130e388895e32f
unknown <sthembiso.ngwenyama@gmail.com>
2025-03-21 02:09:42 +0200

    Adding files

--- Ispani/backend/myapp/consumers.py
+++ Ispani/backend/myapp/consumers.py
@@ -1,22 +1,25 @@
 # consumers.py
+from email.headerregistry import Group
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message
 
+# Update your consumers.py
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name = self.scope['url_route']['kwargs']['group_name']
-        self.room_group_name = f"chat_{self.group_name}"
+        self.room_name = self.scope['url_route']['kwargs']['group_id']
+        self.room_group_name = f'chat_{self.room_name}'
 
-        # Join the room group
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave the room group
+        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -24,27 +27,80 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
 
         # Save message to database
-        group = Group.objects.get(name=self.group_name)
-        Message.objects.create(user=self.scope['user'], group=group, content=message)
-
+        # This would need to be handled asynchronously in production
+        
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': username
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        username = event['username']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username
+        }))
+
+# Add a new consumer for private messages
+class PrivateChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.room_group_name = f'user_{self.user_id}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        recipient_id = data['recipient_id']
+
+        # Send message to recipient's group
+        recipient_group = f'user_{recipient_id}'
+        await self.channel_layer.group_send(
+            recipient_group,
+            {
+                'type': 'private_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    # Receive message from group
+    async def private_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
         }))
